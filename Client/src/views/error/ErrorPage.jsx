b8d37b5b93d40cdbf9215a965800330c47076fac
DiazAdriel0 <diazadriel0@gmail.com>
2023-06-23 16:32:16 -0300

    M3 completed

--- Client/src/views/error/ErrorPage.jsx
+++ Client/src/views/error/ErrorPage.jsx
@@ -1,7 +1,7 @@
 export default function ErrorPage(){
     return (
         <div>
-            <p>Error</p>
+            <p>¡Ups! Parece que algo se desvió en una dimensión equivocada. Pero no te preocupes, nuestro equipo multidimensional de ingenieros ya está trabajando en ello. Mientras tanto, ¿por qué no disfrutas de un poco de interdimensional cable?</p>
         </div>
     )
 }
