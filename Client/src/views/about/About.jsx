c21d3971676904d561f870175658dfa02a5c0ad8
DiazAdriel0 <diazadriel0@gmail.com>
2023-06-23 16:30:24 -0300

    M3 completed

--- Client/src/views/about/About.jsx
+++ Client/src/views/about/About.jsx
@@ -3,7 +3,8 @@ import style from "./about.module.css"
 export default function About() {
     return (
         <div className={style.container}>
-            <p>About Me</p>
+            <h1>Explorando el Universo de Rick and Morty: Un Proyecto Impulsado por HTML, CSS, JS, React, Redux, Node, Express y Jest</h1>
+            <p>Bienvenidos a mi proyecto sobre Rick and Morty. Aunque no soy un seguidor habitual de la serie, he asumido el desafío de crear esta página web utilizando HTML, CSS, JavaScript, React, Redux, Node, Express y Jest como parte de un proyecto propuesto por una academia. A través de este proyecto, he tenido la oportunidad de adentrarme en el fascinante mundo de Rick and Morty, explorando su popularidad, tramas y su impacto en la cultura popular. Utilizando estas tecnologías, he creado una página web informativa y completa que busca ofrecer una perspectiva imparcial y objetiva sobre la serie, centrándome en su influencia en el ámbito del entretenimiento y las opiniones de la audiencia. Espero que esta página sea una fuente de referencia para aquellos que deseen conocer más sobre Rick and Morty, y estoy abierto a recibir comentarios y sugerencias para mejorarla. ¡Gracias por visitar mi proyecto!</p>
         </div>
     )
 }
